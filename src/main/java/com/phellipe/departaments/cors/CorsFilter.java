48cd58999dc24b9fe71f4e288c02bd7fb7599327
Phellipe Rodrigues <31316802+phelliperodrigues@users.noreply.github.com>
2020-08-28 14:06:16 -0300

    Update CorsFilter.java

--- src/main/java/com/phellipe/departaments/cors/CorsFilter.java
+++ src/main/java/com/phellipe/departaments/cors/CorsFilter.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter  implements Filter {
 
-    private String origin = "http://localhost:4200";
+    private String origin = "*";
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -27,10 +27,9 @@ public class CorsFilter  implements Filter {
 
 
         if ("OPTIONS".equals(req.getMethod()) && origin.equals(req.getHeader("Origin"))){
-            res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, PATH, OPTIONS");
-            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             res.setHeader("Access-Control-Max-Age", "3600");
-
+            res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+            res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
             res.setStatus(HttpServletResponse.SC_OK);
         }else {
             chain.doFilter(request, response);
